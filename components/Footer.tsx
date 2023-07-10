9b085ea3c003074eca9f42fc7b8a52a3baad3d4d
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-11 03:39:29 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { footerLinks } from "@/constant";
-import FooterCol from "@/utils/FooterCol";
+import FooterCol from "@/components/FooterCol";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
