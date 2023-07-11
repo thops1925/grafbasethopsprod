e3bf8748df48423eb325e27e54190a145b001fcc
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-11 14:05:50 +0800

    div

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const ProjectCard = ({ id, image, name, avatarUrl, userId, title }: Props) => {
     return (
-        <section className='flex items-center justify-center flex-col rounded-sm drop-shadow-md' key={id}>
+        <div className='flex items-center justify-center flex-col rounded-sm drop-shadow-md' key={id}>
             <Link
                 href={`/project/${id}`}
                 className='flex items-center justify-center group relative w-full h-full'>
@@ -67,7 +67,7 @@ const ProjectCard = ({ id, image, name, avatarUrl, userId, title }: Props) => {
                     </div> */}
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
