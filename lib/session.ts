efad41940448484ca9db763e61f359e3614f491f
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-13 01:17:36 +0800

    png

--- lib/session.ts
+++ lib/session.ts
@@ -35,7 +35,7 @@ export const authOptions: NextAuthOptions = {
   },
   theme: {
     colorScheme: "light",
-    logo: "/logo.svg",
+    logo: "/thops3.png",
   },
   callbacks: {
     async session({ session }) {
