4e2c05990b79e5a42f56161adf87a3fb26fe1fa6
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-12 16:21:22 +0800

    actruin

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,24 @@
 import { GraphQLClient } from "graphql-request";
 
-import { createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery } from "@/graphql";
+import {
+    createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation,
+    getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery
+} from "@/graphql";
+
 import { ProjectForm } from "@/common";
 
 const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
-const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
-const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
+const apiUrl = isProduction
+    ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL
+    || ''
+    : 'http://127.0.0.1:4000/graphql';
+const apiKey = isProduction
+    ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY
+    || ''
+    : 'letmein';
+const serverUrl = isProduction
+    ? process.env.NEXT_PUBLIC_SERVER_URL
+    : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl);
 
@@ -19,19 +31,7 @@ export const fetchToken = async () => {
     }
 };
 
-export const uploadImage = async (imagePath: string) => {
-    try {
-        const response = await fetch(`${serverUrl}/api/upload`, {
-            method: "POST",
-            body: JSON.stringify({
-                path: imagePath,
-            }),
-        });
-        return response.json();
-    } catch (err) {
-        throw err;
-    }
-};
+
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
     try {
@@ -127,4 +127,19 @@ export const getUserProjects = async (id: string, last?: number) => {
 export const getUser = async (email: string) => {
     client.setHeader("x-api-key", apiKey);
     return makeGraphQLRequest(getUserQuery, { email });
+};
+
+
+export const uploadImage = async (imagePath: string) => {
+    try {
+        const response = await fetch(`${serverUrl}/api/upload`, {
+            method: "POST",
+            body: JSON.stringify({
+                path: imagePath,
+            }),
+        });
+        return response.json();
+    } catch (err) {
+        throw err;
+    }
 };
