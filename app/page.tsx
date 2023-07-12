6d59eee113c455c76ee1aa8775e0cff0851f1164
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-12 21:02:37 +0800

    remove session

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,6 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  const session = getCurrentUser()
   const data = await fetchAllProjects(category, endcursor) as ProjectSearch
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
