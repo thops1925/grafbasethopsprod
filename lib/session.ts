bb4cf9941ce5974026a5be76f0731f7e12b2b3ca
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-15 22:37:37 +0800

    session

--- lib/session.ts
+++ lib/session.ts
@@ -11,8 +11,8 @@ import { SessionInterface, UserProfile } from "@/common.types";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
     }),
   ],
   jwt: {
@@ -35,7 +35,7 @@ export const authOptions: NextAuthOptions = {
   },
   theme: {
     colorScheme: "light",
-    logo: "/logo.svg",
+    logo: "/thops2.png",
   },
   callbacks: {
     async session({ session }) {
